8aea2205eddc6dae66f459253085ae3229bead38
Martin Bächtold <martin@baechtold.me>
2024-10-05 17:51:09 +0200

    Display a cat image in fullscreen

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
 from flask import Flask
-from datetime import datetime
+
+from flask import render_template
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
+    render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
-
